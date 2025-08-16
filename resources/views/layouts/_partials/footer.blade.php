abaa5194d77b8db54a04262d09707e3b140809e3
rafiazikri <rafiazikri144@gmail.com>
2025-08-16 14:28:27 +0700

    revisi readme dan dan footer

--- resources/views/layouts/_partials/footer.blade.php
+++ resources/views/layouts/_partials/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; 2024-{{ date('Y') }} <a href="#">Sertifikat App</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 3.1.0
     </div>
 </footer>
